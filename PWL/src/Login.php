74cd7cecaf108a67abfd78a31fe1ebe2d77e1bbc
Hafiz <hafizyanuaribrahim@next-ti.co.id>
2022-07-19 08:43:29 +0700

    add css and method

--- PWL/src/Login.php
+++ PWL/src/Login.php
@@ -15,15 +15,15 @@
 <body>
     <div class="container-fluid text-center centeringform">
         <div class="row justify-content-center">
-            <form class="col-3">
-                <h1>Sign In</h1>
+            <form class="col-3 pb-3 border border-secondary rounded" method="POST">
+                <h1>SIGN IN</h1>
                 <p>
-                <label class="sr-only" for="">Email Address</label>
-                <input class="form-control" type="email" placeholder="Email Address" required autofocus>
+                <label class="sr-only" for="">NIK</label>
+                <input name="inpNik" class="form-control" type="text" placeholder="NIK" required autofocus>
                 </p>
                 <p>
                 <label class="sr-only" for="">Password</label>
-                <input class="form-control" type="email" placeholder="Password" required>
+                <input name="inpPassword" class="form-control" type="password" placeholder="Password" required>
                 </p>
                 <button class="btn btn-primary btn-block" type="sumbit">Sign In</button>
             </form>
