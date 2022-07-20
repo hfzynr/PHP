019217eee8da7012bdc76037a160069a865c3443
Hafiz <hafizyanuaribrahim@next-ti.co.id>
2022-07-20 16:56:37 +0700

    finish create session

--- PWL/index.php
+++ PWL/index.php
@@ -1,61 +1,31 @@
 <?php
     session_start();
-
-    if(isset($_POST['inpNik']) && isset($_POST['inpPassword'])){
-        
-        require './src/Koneksi/Koneksi.php';
-
-        //buka koneksi
-        $conn = open_connection();
-
-        //baca inputan
-        $username = $_POST['inpNik'];
-        $password = $_POST['inpPassword'];
-
-        //bikin query SQL
-        $query = "SELECT * FROM user WHERE username = '$username' AND password = MD5('$password')";
-
-        //eksekusi query
-        $result = mysqli_query($conn, $query);
-
-        //cek hasil, jika ada data, maka simpan session dan redirect ke index
-        if($isi = mysqli_fetch_assoc($result)){
-            //simpan session
-            $_SESSION['inpNik'] = $isi['inpNik'];
-            //redirect
-            header("Location: http://127.0.0.1/PHP/PWL/src/Contents/Dashboard.php");
-        } else {
-            echo '<div class="alert alert-danger" role="alert"> Username atau Password Salah! </div>';
-        }   
+    if(isset($_SESSION['inpNik']) && !empty($_SESSION['inpNik'])) {
+        //Masuk Session
+    } else {
+        header("Location: ./login.php");
     }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <?php
-        include '.\headscript.php';
-    ?>
-    <title>Login Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include './src/Contents/Headscript.php' ?>
+    <title>Dashboard Kelompok 2</title>
 </head>
+<?php include './src/Contents/Header.php' ?>
 <body>
-    <div class="container-fluid text-center centeringform">
-        <div class="row justify-content-center">
-            <form class="col-3 pb-3 border border-secondary rounded" method="POST">
-                <h1>SIGN IN</h1>
-                <p>
-                <label class="sr-only" for="">NIK</label>
-                <input name="inpNik" class="form-control" type="text" placeholder="NIK" required autofocus>
-                </p>
-                <p>
-                <label class="sr-only" for="">Password</label>
-                <input name="inpPassword" class="form-control" type="password" placeholder="Password" required>
-                </p>
-                <button class="btn btn-primary btn-block" type="sumbit">Sign In</button>
-            </form>
-        </div>
+    <div class="jumbotron jumbotron-fluid text-center">
+    <h1 class="display-4">Hello, world!</h1>
+    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <hr class="my-4">
+    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </p>
     </div>
 </body>
+<?php include './src/Contents/Footer.php' ?>
 </html>
---
+++ PWL/src/Contents/Header.php
@@ -0,0 +1,21 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a class="navbar-brand" href="./../../index.php">Home</a>
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Input Nilai<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="$">About Us</a>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="./logout.php">Logout</a>
+        </li>
+        </ul>
+    </div>
+    </nav>
+</header>
