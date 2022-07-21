666dd3d16283207313751c43944a7de5d8389c3a
Hafiz <hafizyanuaribrahim@next-ti.co.id>
2022-07-21 16:25:36 +0700

    .

--- PWL/src/Contents/Header.php
+++ PWL/src/Contents/Header.php
@@ -1,19 +1,35 @@
 <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <a class="navbar-brand" href="./../../index.php">Home</a>
+        <a class="navbar-brand" href="./">Kelompok 2</a>
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
         <li class="nav-item active">
             <a class="nav-link" href="#">Input Nilai<span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item active">
-            <a class="nav-link" href="$">About Us</a>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Report
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="./">Rekap 1</a>
+            <a class="dropdown-item" href="./">Rekap 2</a>
+            <a class="dropdown-item" href="./">Rekap 3</a>
+            </div>
         </li>
-        <li class="nav-item active">
-            <a class="nav-link" href="./logout.php">Logout</a>
+        </ul>
+        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+        <li class="nav-item dropdown">
+            <button class="btn btn-outline-light dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Setting
+            </button>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="/src/Pages/Profile.php">Profile</a>
+            <a class="dropdown-item" href="./logout.php">Logout</a>
+            <a class="dropdown-item" href="/src/Pages/AboutUs.php">About Us</a>
+            </div>
         </li>
         </ul>
     </div>
