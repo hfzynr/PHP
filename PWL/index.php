57ac04cf07b2a506ff3d10167ced6b045a2084bc
Hafiz Yanuar Ibrahim <hafizyanuar95@gmail.com>
2022-08-08 08:41:02 +0700

    modif index

--- PWL/index.php
+++ PWL/index.php
@@ -18,13 +18,13 @@
 <?php include './src/Contents/Header.php' ?>
 <body>
     <div class="jumbotron jumbotron-fluid text-center">
-    <h1 class="display-4">Hello, world!</h1>
-    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <h1 class="display-4">Selamat datang di Aplikasi Sekolah!</h1>
+    <p class="lead">Ini adalah Aplikasi Prototype untuk input berbagai kebutuhan sekolah.</p>
     <hr class="my-4">
-    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-    <p class="lead">
+    <p>Aplikasi ini di support oleh PHP dan MySQL.</p>
+    <!-- <p class="lead">
         <a class="btn btn-primary btn-lg" href="./" role="button">Learn more</a>
-    </p>
+    </p> -->
     </div>
 </body>
 <?php include './src/Contents/Footer.php' ?>
